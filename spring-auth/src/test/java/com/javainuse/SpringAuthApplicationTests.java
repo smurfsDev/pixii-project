d673ee6a5647e2b33de041912fb566a4e81e26c4
kamkoum-sabrine <kamkoumsabrine30@gmail.com>
2022-11-05 13:11:24 +0100

    [spring] init spring application

--- spring-auth/src/test/java/com/javainuse/SpringAuthApplicationTests.java
+++ spring-auth/src/test/java/com/javainuse/SpringAuthApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApplicationTests {
+class SpringAuthApplicationTests {
 
 	@Test
 	void contextLoads() {
