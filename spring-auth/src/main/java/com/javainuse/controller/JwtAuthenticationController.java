7c769df0fb33986368719a6c8ef9b62fcca7e040
kamkoum-sabrine <kamkoumsabrine30@gmail.com>
2022-11-05 18:21:59 +0100

    [spring] remove unused import

--- spring-auth/src/main/java/com/javainuse/controller/JwtAuthenticationController.java
+++ spring-auth/src/main/java/com/javainuse/controller/JwtAuthenticationController.java
@@ -1,7 +1,5 @@
 package com.javainuse.controller;
 
-import java.util.Objects;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
