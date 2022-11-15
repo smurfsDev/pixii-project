11b41f4db2cac78240a80d439b0f627a0f710c60
raed-charrad <raed.charrad91@gmail.com>
2022-11-15 23:03:04 +0100

    add register and login routes

--- Angular/src/app/app.routes.ts
+++ Angular/src/app/app.routes.ts
@@ -1,7 +1,12 @@
 import { Routes } from '@angular/router';
 import { ClaimsComponent } from './pages/claims/claims.component';
+import { LoginComponent } from './pages/account/login/login.component';
+import { RegisterComponent } from './pages/account/register/register.component';
 import { HomeComponent } from './pages/home/home.component';
+
 export const AppRoutes: Routes = [
 	{ path: '', component: HomeComponent },
 	{ path: 'claims', component: ClaimsComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
 ];
