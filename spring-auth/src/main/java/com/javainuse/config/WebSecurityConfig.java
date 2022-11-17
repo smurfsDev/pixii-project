fd2a5444bacde12c61c42e8a2efb73680b52218f
raed-charrad <raed.charrad91@gmail.com>
2022-11-17 22:13:08 +0100

    [spring] add permition

--- spring-auth/src/main/java/com/javainuse/config/WebSecurityConfig.java
+++ spring-auth/src/main/java/com/javainuse/config/WebSecurityConfig.java
@@ -55,7 +55,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 // dont authenticate this particular request
                 .authorizeRequests()
                 .antMatchers("/authenticate").permitAll()
-                .antMatchers("/register").permitAll().
+                .antMatchers("/register").permitAll()
+                .antMatchers("/roles").permitAll().
                 // all other requests need to be authenticated
                 anyRequest().authenticated().and().
                 // make sure we use stateless session; session won't be used to
