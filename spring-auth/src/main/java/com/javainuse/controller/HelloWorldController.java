32be929380bd3589e6d06761c75e6bec74e0c0dd
kamkoum-sabrine <kamkoumsabrine30@gmail.com>
2022-11-17 16:29:44 +0100

    [spring] login with email not with name

--- spring-auth/src/main/java/com/javainuse/controller/HelloWorldController.java
+++ spring-auth/src/main/java/com/javainuse/controller/HelloWorldController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
 	@RequestMapping({ "/hello" })
 	public String firstPage() {
+
 		return "Hello World";
 	}
 }
