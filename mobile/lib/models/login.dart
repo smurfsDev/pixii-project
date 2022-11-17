f5e2b9c6675ed603de4188a0ab552123b6ded0f4
youssefchlendi <youssef.chlendi@gmail.com>
2022-11-18 00:43:33 +0100

    create models

---
+++ mobile/lib/models/login.dart
@@ -0,0 +1,31 @@
+// To parse this JSON data, do
+//
+//     final loginResponse = loginResponseFromJson(jsonString);
+
+import 'dart:convert';
+import 'package:mobile/models/user.dart';
+
+LoginResponse loginResponseFromJson(String str) =>
+    LoginResponse.fromJson(json.decode(str));
+
+String loginResponseToJson(LoginResponse data) => json.encode(data.toJson());
+
+class LoginResponse {
+  LoginResponse({
+    required this.user,
+    required this.token,
+  });
+
+  User user;
+  String token;
+
+  factory LoginResponse.fromJson(Map<String, dynamic> json) => LoginResponse(
+        user: User.fromJson(json["user"]),
+        token: json["token"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "user": user.toJson(),
+        "token": token,
+      };
+}
