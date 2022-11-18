78ebec3a705210e91342a05c4299e64853980532
youssefchlendi <youssef.chlendi@gmail.com>
2022-11-18 09:59:53 +0100

    create main layout

--- mobile/lib/ui/pages/auth/login.dart
+++ mobile/lib/ui/pages/auth/login.dart
@@ -20,20 +20,7 @@ class _LoginPageState extends State<Login> {
   @override
   Widget build(BuildContext context) {
     final authService = Provider.of<AuthService>(context);
-    return Container(
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          // begin: Alignment.centerLeft,
-          // end: Alignment.centerRight,
-          stops: const [0.1, 0.5, 1],
-          colors: const [
-            Color.fromRGBO(33, 39, 45, 1),
-            Color.fromRGBO(44, 55, 91, 1),
-            Color.fromRGBO(33, 39, 45, 1),
-          ],
-        ),
-      ),
-      child: Scaffold(
+    return Scaffold(
         resizeToAvoidBottomInset: false,
         backgroundColor: Colors.transparent,
         body: LayoutBuilder(
@@ -211,7 +198,6 @@ class _LoginPageState extends State<Login> {
             );
           },
         ),
-      ),
     );
   }
 
