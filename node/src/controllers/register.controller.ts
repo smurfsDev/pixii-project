26af2523d1f8726e4acb192ec33108c68b0bbd9c
kamkoum-sabrine <kamkoumsabrine30@gmail.com>
2022-11-18 12:48:55 +0100

    [node] create user method in register controller

---
+++ node/src/controllers/register.controller.ts
@@ -0,0 +1,33 @@
+
+import { Request, Response } from 'express';
+import User from "../models/user.model";
+
+export const findAll = (req: Request, res: Response) => {
+    const search = req.query.search || '';
+    const page: number = parseInt(req.query.page?.toString() || '1');
+    const size: number = parseInt(req.query.size?.toString() || '5');
+
+    User.paginate({
+        message: { $regex: ".*(?i)" + search + ".*" },
+        subject: { $regex: ".*(?i)" + search + ".*" }
+    }, { page: page, limit: size }, (err: any, users: any) => {
+        if (err) res.status(500).send(err);
+        else res.send(users);
+    });
+
+};
+// create
+export const create = (req: Request, res: Response) => {
+    const user = new User(req.body);
+
+    user.save((err: any) => {
+        if (err) return res.status(500).send(err);
+        else {
+            const user = User.findByIdAndUpdate(req.body.user, (err: any, user: any) => {
+                if (err) return res.status(500).send(err);
+                else return res.status(200).send(user);
+            });
+        };
+    })
+
+};
