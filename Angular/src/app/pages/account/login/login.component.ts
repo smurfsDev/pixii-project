8ded319d29e64cb7d1b0d5a2fca23b8d27faac95
raed-charrad <raed.charrad91@gmail.com>
2022-11-19 13:17:39 +0100

    [angular] add state after login

--- Angular/src/app/pages/account/login/login.component.ts
+++ Angular/src/app/pages/account/login/login.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 import { User } from 'src/app/models/user';
 import { LoginService } from 'src/app/service/account/login.service';
+import { Store } from '@ngxs/store';
+import { SetIsAuthenticated, SetToken, SetUser } from 'src/app/store/auth/actions';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -10,7 +12,7 @@ import { LoginService } from 'src/app/service/account/login.service';
 export class LoginComponent implements OnInit {
   hide = true;
   loginForm : FormGroup;
-  user : User = new User( '','','',null,{});
+  user : User = new User( '','','',null);
   username = new FormControl('', [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,10}$')]);
   password = new FormControl('', [Validators.required,
                                   Validators.minLength(8),
@@ -37,7 +39,8 @@ export class LoginComponent implements OnInit {
     }
     return '';
   }
-  constructor(private formBuilder: FormBuilder , private LoginService: LoginService) {
+  constructor(private formBuilder: FormBuilder , private LoginService: LoginService, private store: Store)
+     {
     this.loginForm = this.formBuilder.group({
       username: this.username,
       password: this.password
@@ -46,6 +49,12 @@ export class LoginComponent implements OnInit {
 
   ngOnInit(): void {
     // error messages of the form hide
+    localStorage.clear();
+    // this.store.dispatch([
+    //   new SetToken('Ahaha'),
+    //   new SetUser(new User("1", "youssef", "email", "role")),
+    //   new SetIsAuthenticated(true)
+    // ]);
 
   }
   onSubmit() {
@@ -53,7 +62,11 @@ export class LoginComponent implements OnInit {
       this.LoginService.login(this.loginForm.value).subscribe((data: any) => {
         console.log(data);
         this.user = data;
-
+        this.store.dispatch([
+          new SetToken(data.token),
+          new SetUser(new User(data.user.id, data.user.username, data.user.name, data.user.roles)),
+          new SetIsAuthenticated(true)
+        ]);
       }, (error: any) => {
         console.log(error);
       }
