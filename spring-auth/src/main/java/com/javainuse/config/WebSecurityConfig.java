f1dce06942fb5a7de33d9b46b1e4ccb2181388c1
Mohamed Nour Bensalah <mednourbensalah@gmail.com>
2022-11-20 15:45:11 +0100

    [Spring] verify user email

--- spring-auth/src/main/java/com/javainuse/config/WebSecurityConfig.java
+++ spring-auth/src/main/java/com/javainuse/config/WebSecurityConfig.java
@@ -55,7 +55,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 // dont authenticate this particular request
                 .authorizeRequests()
                 .antMatchers("/authenticate").permitAll()
-                .antMatchers("/register").permitAll().
+                .antMatchers("/register").permitAll()
+                .antMatchers("/verify").permitAll()
+                .antMatchers("/forgot_password").permitAll()
+                .antMatchers("/reset_password").permitAll().
                 // all other requests need to be authenticated
                 anyRequest().authenticated().and().
                 // make sure we use stateless session; session won't be used to
--- spring-auth/src/main/java/com/javainuse/repository/UserRepository.java
+++ spring-auth/src/main/java/com/javainuse/repository/UserRepository.java
@@ -7,9 +7,27 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import com.javainuse.entities.User;
+import com.jayway.jsonpath.Option;
 
 @EnableJpaRepositories
 public interface UserRepository extends JpaRepository<User, Integer> {
+    
+    // @Query(" select u from User u where u.email = ?1")
+    // Optional<User> findUserWithEmail(String email);
+
     @Query(" select u from User u where u.username = ?1")
     Optional<User> findUserWithName(String username);
+
+    @Query("SELECT u FROM User u WHERE u.verificationCode = ?1")
+    public User findByVerificationCode(String code);
+       
+    @Query("SELECT u FROM User u WHERE u.email = ?1")
+    public User findByEmail(String email);
+
+    @Query("SELECT u FROM User u WHERE u.email = ?1")
+    Optional<User> findUserByEmail(String email);
+    
+    // public User findByResetPasswordToken(String token);
+
+
 }
