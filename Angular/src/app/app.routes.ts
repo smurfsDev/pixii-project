b5c16f73b06d98e85b2647b27ce781a97a733936
kamkoum-sabrine <kamkoumsabrine30@gmail.com>
2022-11-22 20:41:06 +0100

    [angular] try to secure routes

--- Angular/src/app/app.routes.ts
+++ Angular/src/app/app.routes.ts
@@ -10,7 +10,7 @@ export const AppRoutes: Routes = [
   {
     path: '',
     component: HomeComponent,
-    canActivate: [IsAuthenticatedGuard]
+    // canActivate: [IsAuthenticatedGuard]
   },
   {
     path: 'claims',
--- Angular/src/app/is-authenticated.guard.ts
+++ Angular/src/app/is-authenticated.guard.ts
@@ -1,13 +1,19 @@
 import { Injectable } from "@angular/core";
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from "@angular/router";
-import { Observable } from "rxjs";
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
+import { Observable, tap } from "rxjs";
+import { LoginService } from "./service/account/login.service";
+import { RegisterService } from "./service/account/register.service";
 
 @Injectable({
     providedIn: 'root'
 })
 
 export class IsAuthenticatedGuard implements CanActivate {
+
+    constructor(private loginService: LoginService, private router: Router) { }
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
-        return true;
+        return true
+
+        // return localStorage.getItem("")
     }
 }
