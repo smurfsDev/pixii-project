94f7991ff6e365eb818dc69c955d78405822d93c
kamkoum-sabrine <kamkoumsabrine30@gmail.com>
2022-11-22 19:44:19 +0100

    [angular] add is-authenticated.guard to secure routes

---
+++ Angular/src/app/is-authenticated.guard.ts
@@ -0,0 +1,13 @@
+import { Injectable } from "@angular/core";
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from "@angular/router";
+import { Observable } from "rxjs";
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class IsAuthenticatedGuard implements CanActivate {
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+        return true;
+    }
+}
