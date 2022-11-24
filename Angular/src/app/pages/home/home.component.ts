590d7eb2eff853269fba0304ce62f2d4a3eafc8f
kamkoum-sabrine <kamkoumsabrine30@gmail.com>
2022-11-24 18:37:41 +0100

    [angular] add board to home component

--- Angular/src/app/pages/home/home.component.ts
+++ Angular/src/app/pages/home/home.component.ts
@@ -5,7 +5,7 @@ import { Store } from '@ngxs/store';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
 
