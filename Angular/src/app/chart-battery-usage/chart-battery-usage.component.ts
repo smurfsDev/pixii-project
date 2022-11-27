b0927c4ace04ba4238f4e9757df36416b0076fe7
kamkoum-sabrine <kamkoumsabrine30@gmail.com>
2022-11-27 13:45:55 +0100

    [Angular] changing dashboard style

--- Angular/src/app/chart-battery-usage/chart-battery-usage.component.ts
+++ Angular/src/app/chart-battery-usage/chart-battery-usage.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { EChartsOption } from 'echarts';
 
 @Component({
@@ -6,41 +6,122 @@ import { EChartsOption } from 'echarts';
   templateUrl: './chart-battery-usage.component.html',
   styleUrls: ['./chart-battery-usage.component.css']
 })
-export class ChartBatteryUsageComponent implements OnInit {
+export class ChartBatteryUsageComponent implements OnInit, OnDestroy {
 
+
+  options: any;
+  updateOptions: any;
+
+  private oneDay = 24 * 3600 * 1000;
+  private now!: Date;
+  private value!: number;
+  private data!: any[];
+  private timer: any;
 
   constructor() { }
-  ngOnInit(): void {
 
-  }
-  // options = {
-  //   chart: {
-  //     type: 'line'
-  //   },
-  //   series: [{
-  //     name: 'sales',
-  //     data: [30, 40, 35, 50, 49, 60, 70, 91, 125]
-  //   }],
-  //   xaxis: {
-  //     categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999]
-  //   }
-  // }
-  chart = "Chart"
-  chartOption: EChartsOption = {
-    xAxis: {
-      type: 'category',
-      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-    },
-    yAxis: {
-      type: 'value',
-    },
-    series: [
-      {
-        data: [820, 932, 901, 934, 1290, 1330, 1320],
-        type: 'line',
+  ngOnInit(): void {
+    // generate some random testing data:
+    this.data = [];
+    this.now = new Date(2020, 4, 30);
+    this.value = Math.random() * 100;
+
+    for (let i = 0; i < 100; i++) {
+      this.data.push(this.randomData());
+    }
+
+    // initialize chart options:
+    this.options = {
+      title: {
+        text: 'Battery Usage'
       },
-    ],
-  };
+      tooltip: {
+        trigger: 'axis',
+        formatter: (params: any) => {
+          params = params[0];
+          const date = new Date(params.name);
+          return date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear() + ' : ' + params.value[1];
+        },
+        axisPointer: {
+          animation: false
+        }
+      },
+      xAxis: {
+        type: 'time',
+        splitLine: {
+          show: false
+        }
+      },
+      yAxis: {
+        type: 'value',
+        boundaryGap: [0, '100%'],
+        splitLine: {
+          show: false
+        }
+      },
+      series: [{
+        name: 'Mocking Data',
+        type: 'line',
+        showSymbol: false,
+        emphasis: {
+          line: false,
+        },
+        data: this.data
+      }]
+    };
+
+    // Mock dynamic data:
+    this.timer = setInterval(() => {
+      for (let i = 0; i < 5; i++) {
+        this.data.shift();
+        this.data.push(this.randomData());
+      }
+
+      // update series data:
+      this.updateOptions = {
+        series: [{
+          data: this.data
+        }]
+      };
+    }, 1000);
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.timer);
+  }
+
+  randomData() {
+    this.now = new Date(this.now.getTime() + this.oneDay);
+    this.value = this.value + Math.random() * 21 - 10;
+    return {
+      name: this.now.toString(),
+      value: [
+        [this.now.getFullYear(), this.now.getMonth() + 1, this.now.getDate()].join('/'),
+        Math.round(this.value)
+      ]
+    };
+  }
+  // constructor() { }
+  // ngOnInit(): void {
+
+  // }
+
+  // chart = "Chart"
+  // chartOption: EChartsOption = {
+  //   xAxis: {
+  //     type: 'category',
+  //     data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+  //   },
+  //   yAxis: {
+  //     type: 'value',
+  //   },
+  //   series: [
+  //     {
+  //       data: [820, 932, 901, 934, 1290, 1330, 1320],
+  //       type: 'line',
+  //     },
+  //   ],
+  // };
 
 
   // options: any;
