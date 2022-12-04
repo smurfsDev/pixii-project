b73c763e0f9cd9aa94b3ac9de5675b32f58bc09e
kamkoum-sabrine <kamkoumsabrine30@gmail.com>
2022-12-04 21:21:37 +0100

    [flutter] create bottom navigation bar

---
+++ mobile/lib/ui/layout/bottom_navigation_bar.dart
@@ -0,0 +1,65 @@
+// ignore_for_file: must_be_immutable
+
+import 'package:mobile/imports.dart';
+
+class bottomNav extends StatefulWidget {
+  @override
+  // ignore: no_logic_in_create_state
+  State<bottomNav> createState() => _bottomNav();
+}
+
+class _bottomNav extends State<bottomNav> {
+  int _selectedIndex = 0;
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text('Home Page',
+        style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold)),
+    Text('Search Page',
+        style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold)),
+    Text('Profile Page',
+        style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold)),
+    Text('Profile Page',
+        style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold)),
+  ];
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+        backgroundColor: const Color.fromARGB(255, 19, 27, 54),
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.electric_scooter_outlined,
+                color: Color.fromARGB(255, 249, 249, 249)),
+            label: 'Management',
+            backgroundColor: Color.fromARGB(255, 19, 27, 54),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.location_on,
+                color: Color.fromARGB(255, 249, 249, 249)),
+            label: 'Localization',
+          ),
+          BottomNavigationBarItem(
+            icon:
+                Icon(Icons.settings, color: Color.fromARGB(255, 249, 249, 249)),
+            label: 'Settings',
+            backgroundColor: Color.fromARGB(255, 19, 27, 54),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.notifications,
+                color: Color.fromARGB(255, 249, 249, 249)),
+            label: 'Notification',
+            backgroundColor: Color.fromARGB(255, 19, 27, 54),
+          ),
+        ],
+        type: BottomNavigationBarType.shifting,
+        currentIndex: _selectedIndex,
+        selectedItemColor: Color.fromARGB(255, 234, 234, 235),
+        iconSize: 40,
+        onTap: _onItemTapped,
+        elevation: 5);
+  }
+}
--- mobile/lib/ui/pages/home/dashboard.dart
+++ mobile/lib/ui/pages/home/dashboard.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:mobile/imports.dart';
+import 'package:mobile/ui/layout/bottom_navigation_bar.dart';
 import 'package:mobile/ui/layout/navigation_menu.dart';
 import 'package:mobile/ui/layout/scooter_management_layout.dart';
 
@@ -18,114 +19,117 @@ class _Dashboard extends State<Dashboard> {
   String password = "";
   User user;
   _Dashboard(this.user);
+
   @override
   Widget build(BuildContext context) {
     return (MaterialApp(
         debugShowCheckedModeBanner: false,
         home: Scaffold(
-            appBar: AppBar(
-              title: const Padding(
-                padding: EdgeInsets.only(top: 5),
-                child: Text('Shawdow\nControl Panel'),
-              ),
-              backgroundColor: const Color.fromARGB(255, 19, 27, 54),
+          appBar: AppBar(
+            title: const Padding(
+              padding: EdgeInsets.only(top: 5),
+              child: Text('Shawdow\nControl Panel'),
             ),
-            body: SingleChildScrollView(
-                child: Container(
-              child: Padding(
-                padding: const EdgeInsets.all(40),
-                child: Column(
-                  mainAxisSize: MainAxisSize.max,
-                  children: [
-                    DecoratedBox(
-                      decoration: const BoxDecoration(
-                        color: Color.fromARGB(255, 252, 253, 255),
-                      ),
-                      child: ListTile(
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(50)),
-                        title: const Text("Tires pressure"),
-                        trailing: PopupMenuButton<String>(
-                          icon: const Icon(Icons.arrow_drop_down_outlined),
-                          padding: EdgeInsets.zero,
-                          onSelected: (value) => showInSnackBar(
-                            "text1",
-                          ),
-                          itemBuilder: (context) => <PopupMenuItem<String>>[
-                            const PopupMenuItem<String>(
-                              value: "text1",
-                              child: Text(
-                                "text1",
-                              ),
-                            ),
-                            const PopupMenuItem<String>(
-                              enabled: false,
-                              child: Text(
-                                "text2",
-                              ),
-                            ),
-                            const PopupMenuItem<String>(
-                              value: "text3",
-                              child: Text(
-                                "text3",
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(40),
-                      child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Image.asset(
-                              'assets/images/avatar.jpg',
-                              fit: BoxFit.fill,
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.only(top: 40),
-                              child: Column(
-                                mainAxisSize: MainAxisSize.max,
-                                children: [
-                                  Container(
-                                    color: const Color.fromARGB(
-                                        255, 254, 254, 254),
-                                    child: SizedBox(
-                                        height: 40,
-                                        width: 600,
-                                        child: ElevatedButton.icon(
-                                          style: ElevatedButton.styleFrom(
-                                              minimumSize: const Size(60, 30),
-                                              fixedSize: const Size(10, 2),
-                                              backgroundColor:
-                                                  const Color.fromARGB(
-                                                      255, 254, 254, 254)),
-                                          label: const Text(
-                                            "Check your front right tyre",
-                                            style: TextStyle(
-                                                color: Color.fromARGB(
-                                                    255, 5, 5, 5)),
-                                          ),
-                                          icon: const Icon(Icons.warning,
-                                              color: Color.fromARGB(
-                                                  255, 234, 26, 26),
-                                              size: 18),
-                                          onPressed: () {},
-                                        )),
-                                  )
-                                ],
-                              ),
-                            ),
-                          ]),
-                    ),
-                    ScooterManagement()
-                  ],
-                ),
-              ),
-            )),
             backgroundColor: const Color.fromARGB(255, 19, 27, 54),
-            drawer: NavDrawerDemo(this.user))));
+          ),
+          body: SingleChildScrollView(
+              child: Container(
+            child: Padding(
+              padding: const EdgeInsets.all(40),
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  DecoratedBox(
+                    decoration: const BoxDecoration(
+                      color: Color.fromARGB(255, 252, 253, 255),
+                    ),
+                    child: ListTile(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(50)),
+                      title: const Text("Tires pressure"),
+                      trailing: PopupMenuButton<String>(
+                        icon: const Icon(Icons.arrow_drop_down_outlined),
+                        padding: EdgeInsets.zero,
+                        onSelected: (value) => showInSnackBar(
+                          "text1",
+                        ),
+                        itemBuilder: (context) => <PopupMenuItem<String>>[
+                          const PopupMenuItem<String>(
+                            value: "text1",
+                            child: Text(
+                              "text1",
+                            ),
+                          ),
+                          const PopupMenuItem<String>(
+                            enabled: false,
+                            child: Text(
+                              "text2",
+                            ),
+                          ),
+                          const PopupMenuItem<String>(
+                            value: "text3",
+                            child: Text(
+                              "text3",
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(40),
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset(
+                            'assets/images/avatar.jpg',
+                            fit: BoxFit.fill,
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 40),
+                            child: Column(
+                              mainAxisSize: MainAxisSize.max,
+                              children: [
+                                Container(
+                                  color:
+                                      const Color.fromARGB(255, 254, 254, 254),
+                                  child: SizedBox(
+                                      height: 40,
+                                      width: 600,
+                                      child: ElevatedButton.icon(
+                                        style: ElevatedButton.styleFrom(
+                                            minimumSize: const Size(60, 30),
+                                            fixedSize: const Size(10, 2),
+                                            backgroundColor:
+                                                const Color.fromARGB(
+                                                    255, 254, 254, 254)),
+                                        label: const Text(
+                                          "Check your front right tyre",
+                                          style: TextStyle(
+                                              color:
+                                                  Color.fromARGB(255, 5, 5, 5)),
+                                        ),
+                                        icon: const Icon(Icons.warning,
+                                            color: Color.fromARGB(
+                                                255, 234, 26, 26),
+                                            size: 18),
+                                        onPressed: () {},
+                                      )),
+                                )
+                              ],
+                            ),
+                          ),
+                        ]),
+                  ),
+                  ScooterManagement()
+                ],
+              ),
+            ),
+          )),
+          backgroundColor: const Color.fromARGB(255, 19, 27, 54),
+          drawer: NavDrawerDemo(this.user),
+          bottomNavigationBar: bottomNav(),
+        )));
   }
 
   showInSnackBar(demoMenuSelected) {}
