688cbce142bf78ab10f4a2404a96df3c540506f1
raed-charrad <raed.charrad91@gmail.com>
2022-12-05 00:56:01 +0100

    [Spring] add findUserAndRole func

--- microservice-application/spring-auth/src/main/java/com/javainuse/repository/UserRepository.java
+++ microservice-application/spring-auth/src/main/java/com/javainuse/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package com.javainuse.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -20,4 +21,7 @@ public interface UserRepository extends JpaRepository<User, Integer> {
 	@Query("SELECT u FROM User u WHERE u.email = ?1")
 	Optional<User> findUserByEmail(String email);
 
+	// list of users and their roles pivot table
+	@Query("SELECT ru FROM User u JOIN UserRole ru ON u.id = ru.user.id Join Role r ON ru.role.id = r.id")
+	List<Object[]> findUserAndRole();
 }
