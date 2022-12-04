4ad2b66686fe6ec58ff66b605feb9a53395d0cf1
kamkoum-sabrine <kamkoumsabrine30@gmail.com>
2022-12-05 00:08:20 +0100

    [flutter] change bottom navigation bar

--- mobile/lib/ui/layout/bottom_navigation_bar.dart
+++ mobile/lib/ui/layout/bottom_navigation_bar.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: must_be_immutable
 
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:mobile/imports.dart';
 
 class bottomNav extends StatefulWidget {
@@ -9,69 +10,19 @@ class bottomNav extends StatefulWidget {
 }
 
 class _bottomNav extends State<bottomNav> {
-  int _selectedIndex = 0;
-  static const List<Widget> _widgetOptions = <Widget>[
-    Text('Home Page',
-        style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold)),
-    Text('Search Page',
-        style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold)),
-    Text('Profile Page',
-        style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold)),
-    Text('Profile Page',
-        style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold)),
-  ];
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(
-        backgroundColor: const Color.fromARGB(255, 19, 27, 54),
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.electric_scooter_outlined,
-              color: Color.fromARGB(255, 249, 249, 249),
-              size: 30,
-            ),
-            label: 'Management',
-            backgroundColor: Color.fromARGB(255, 19, 27, 54),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.location_on,
-              color: Color.fromARGB(255, 249, 249, 249),
-              size: 30,
-            ),
-            label: 'Localization',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.settings,
-              color: Color.fromARGB(255, 249, 249, 249),
-              size: 30,
-            ),
-            label: 'Settings',
-            backgroundColor: Color.fromARGB(255, 19, 27, 54),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.notifications,
-              color: Color.fromARGB(255, 249, 249, 249),
-              size: 30,
-            ),
-            label: 'Notification',
-            backgroundColor: Color.fromARGB(255, 10, 17, 38),
-          ),
-        ],
-        type: BottomNavigationBarType.shifting,
-        currentIndex: _selectedIndex,
-        selectedItemColor: Color.fromARGB(255, 234, 234, 235),
-        iconSize: 40,
-        onTap: _onItemTapped,
-        elevation: 5);
+    return CurvedNavigationBar(
+      backgroundColor: Colors.blueAccent,
+      items: const <Widget>[
+        Icon(Icons.bike_scooter, size: 30),
+        Icon(Icons.location_on, size: 30),
+        Icon(Icons.settings, size: 30),
+        Icon(Icons.notifications, size: 30),
+      ],
+      onTap: (index) {
+        //Handle button tap
+      },
+    );
   }
 }
