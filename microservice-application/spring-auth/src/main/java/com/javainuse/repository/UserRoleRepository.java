41ed7b35e143ac454cdb31768a1803ce9d35c899
raed-charrad <raed.charrad91@gmail.com>
2022-12-05 00:56:28 +0100

    [Spring] add findByUserIdAndRoleId func

--- microservice-application/spring-auth/src/main/java/com/javainuse/repository/UserRoleRepository.java
+++ microservice-application/spring-auth/src/main/java/com/javainuse/repository/UserRoleRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import com.javainuse.entities.Role;
@@ -15,5 +16,9 @@ public interface UserRoleRepository extends JpaRepository<UserRole, Integer> {
     Optional<UserRole> findFirstByUserId(int userId);
 
     List<UserRole> findAllByStatus(int i);
+
+    // @Query("SELECT ru FROM User u JOIN UserRole ru ON u.id = ru.user.id Join Role
+    // r ON ru.role.id = r.id WHERE u.id = ?1 AND r.id = ?2")
+    Optional<UserRole> findByUserIdAndRoleId(int userId, Long roleId);
 
 }
