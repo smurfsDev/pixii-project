f043bcabee3315530f7a6779a3f76fb122b2e7ee
kamkoum-sabrine <kamkoumsabrine30@gmail.com>
2022-12-05 00:22:32 +0100

    [flutter] add text to each icon in bottom navbar

--- mobile/lib/global/env.dart
+++ mobile/lib/global/env.dart
@@ -6,15 +6,12 @@ class Environment {
   static String apiUrl = kIsWeb
       ? 'http://localhost:8085' // for Web
       : Platform.isAndroid
-
-          ? 'http://172.17.100.2:8085' // for Android
+          ? 'http://192.168.1.5:8085' // for Android
           : 'http://localhost:8085'; // for Others
-
 
   static String socketUrl = kIsWeb
       ? 'http://localhost:8085' // for Web
       : Platform.isAndroid
-
-          ? 'http://172.17.100.2:8085' // for Android
+          ? 'http://192.168.1.5:8085' // for Android
           : 'http://localhost:8085'; // for Others
 }
--- mobile/lib/ui/layout/bottom_navigation_bar.dart
+++ mobile/lib/ui/layout/bottom_navigation_bar.dart
@@ -10,17 +10,85 @@ class bottomNav extends StatefulWidget {
 }
 
 class _bottomNav extends State<bottomNav> {
+  int _selectedIndex = 0;
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return CurvedNavigationBar(
       backgroundColor: Colors.blueAccent,
-      items: const <Widget>[
-        Icon(Icons.bike_scooter, size: 30),
-        Icon(Icons.location_on, size: 30),
-        Icon(Icons.settings, size: 30),
-        Icon(Icons.notifications, size: 30),
+      items: <Widget>[
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.electric_scooter_outlined,
+                color: _selectedIndex == 0
+                    ? Colors.black
+                    : Color.fromARGB(255, 171, 160, 160)),
+            Text(
+              "Management",
+              style: TextStyle(
+                fontSize: _selectedIndex == 0 ? 0 : 12,
+              ),
+            )
+          ],
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.location_on,
+                color: _selectedIndex == 1
+                    ? Colors.black
+                    : Color.fromARGB(255, 171, 160, 160)),
+            Text(
+              "Localization",
+              style: TextStyle(
+                fontSize: _selectedIndex == 1 ? 1 : 12,
+              ),
+            )
+          ],
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.settings,
+                color: _selectedIndex == 2
+                    ? Colors.black
+                    : Color.fromARGB(255, 171, 160, 160)),
+            Text(
+              "Settings",
+              style: TextStyle(
+                fontSize: _selectedIndex == 2 ? 2 : 12,
+              ),
+            )
+          ],
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.notifications_on_sharp,
+                color: _selectedIndex == 0
+                    ? Colors.black
+                    : Color.fromARGB(255, 171, 160, 160)),
+            Text(
+              "Notifications",
+              style: TextStyle(
+                fontSize: _selectedIndex == 3 ? 3 : 12,
+              ),
+            )
+          ],
+        ),
       ],
       onTap: (index) {
+        _onItemTapped(index);
         //Handle button tap
       },
     );
