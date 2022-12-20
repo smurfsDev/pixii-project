f6b7463d09b2f13c8513a1284922e2f2198984ef
youssefchlendi <youssef.chlendi@gmail.com>
2022-12-20 23:38:51 +0100

    activate buttons

--- mobile/lib/ui/layout/scooter_management_layout.dart
+++ mobile/lib/ui/layout/scooter_management_layout.dart
@@ -1,12 +1,43 @@
 // ignore_for_file: must_be_immutable
 
 import 'package:mobile/imports.dart';
+import 'package:mobile/models/BikeData.dart';
+import 'package:mobile/service/bike.dart';
 
-class ScooterManagement extends StatelessWidget {
+class ScooterManagement extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _ScooterManagmentState();
+}
+
+class _ScooterManagmentState extends State<ScooterManagement> {
+  late BikeData? bike = null;
+  late BikeService bikeService;
+  @override
+  initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+      bikeService = Provider.of<BikeService>(context, listen: false);
+      bike = bikeService.bikeData;
+      print("State" + bikeService.bikeData.toString());
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Column(
+    setState(() {
+      bikeService = Provider.of<BikeService>(context, listen: false);
+      bike = bikeService.bikeData;
+      print("State" + bikeService.bikeData.toString());
+    });
+    // var bikeService = Provider.of<BikeService>(context, listen: false);
+    // bike = bikeService.bikeData;
+    // print(bike!.TheftState);
+    if (bike == null) {
+      return const Center(
+        child: CircularProgressIndicator(),
+      );
+    } else {
+      return Column(
         children: [
           Column(
             children: [
@@ -25,14 +56,30 @@ class ScooterManagement extends StatelessWidget {
                           backgroundColor:
                               const Color.fromARGB(255, 254, 254, 254),
                         ),
-                        onPressed: () {},
+                        onPressed: () {
+                          bikeService.LockBike(true)
+                              .then((value) => setState(() {
+                                    bike = bikeService.bikeData!;
+                                  }));
+                        },
                         child: IconButton(
                           iconSize: 20,
                           padding: const EdgeInsets.all(2),
                           color: const Color.fromARGB(255, 244, 246, 249),
-                          icon: const Icon(Icons.lock_outline,
-                              color: Color.fromARGB(255, 224, 44, 32)),
-                          onPressed: () {},
+                          icon: Icon(
+                            Icons.lock_outline,
+                            color: bike != null
+                                ? !bike!.TheftState
+                                    ? Color.fromARGB(255, 81, 66, 64)
+                                    : Color.fromARGB(255, 224, 44, 32)
+                                : Color.fromARGB(255, 224, 44, 32),
+                          ),
+                          onPressed: () {
+                            bikeService.LockBike(true)
+                                .then((value) => setState(() {
+                                      bike = bikeService.bikeData!;
+                                    }));
+                          },
                         ),
                       ),
                       const Text(
@@ -53,13 +100,27 @@ class ScooterManagement extends StatelessWidget {
                           backgroundColor:
                               const Color.fromARGB(255, 254, 254, 254),
                         ),
-                        onPressed: () {},
+                        onPressed: () {
+                          bikeService.LockBike(false).then((value) => setState(() {
+                                      bike = bikeService.bikeData!;
+                                    }));
+                        },
                         child: IconButton(
                           padding: const EdgeInsets.all(2),
                           color: const Color.fromARGB(255, 244, 246, 249),
-                          icon: const Icon(Icons.lock_open_outlined,
-                              color: Color.fromARGB(255, 224, 44, 32)),
-                          onPressed: () {},
+                          icon: Icon(
+                            Icons.lock_open_outlined,
+                            color: bike != null
+                                ? bike!.TheftState
+                                    ? Color.fromARGB(255, 81, 66, 64)
+                                    : Color.fromARGB(255, 224, 44, 32)
+                                : Color.fromARGB(255, 224, 44, 32),
+                          ),
+                          onPressed: () {
+                            bikeService.LockBike(false).then((value) => setState(() {
+                                      bike = bikeService.bikeData!;
+                                    }));
+                          },
                         ),
                       ),
                       const Text(
@@ -86,13 +147,27 @@ class ScooterManagement extends StatelessWidget {
                           backgroundColor:
                               const Color.fromARGB(255, 254, 254, 254),
                         ),
-                        onPressed: () {},
+                        onPressed: () {
+                          bikeService.ChangeLightState(false).then((value) => setState(() {
+                                      bike = bikeService.bikeData!;
+                                    }));
+                        },
                         child: IconButton(
                           padding: const EdgeInsets.all(2),
                           color: const Color.fromARGB(255, 244, 246, 249),
-                          icon: const Icon(Icons.light_mode_outlined,
-                              color: Color.fromARGB(255, 224, 44, 32)),
-                          onPressed: () {},
+                          icon: Icon(
+                            Icons.light_mode_outlined,
+                            color: bike != null
+                                ? bike!.LightState
+                                    ? Color.fromARGB(255, 81, 66, 64)
+                                    : Color.fromARGB(255, 224, 44, 32)
+                                : Color.fromARGB(255, 224, 44, 32),
+                          ),
+                          onPressed: () {
+                            bikeService.ChangeLightState(false).then((value) => setState(() {
+                                      bike = bikeService.bikeData!;
+                                    }));
+                          },
                         ),
                       ),
                       const Text(
@@ -113,13 +188,27 @@ class ScooterManagement extends StatelessWidget {
                           backgroundColor:
                               const Color.fromARGB(255, 254, 254, 254),
                         ),
-                        onPressed: () {},
+                        onPressed: () {
+                          bikeService.ChangeLightState(true).then((value) => setState(() {
+                                      bike = bikeService.bikeData!;
+                                    }));
+                        },
                         child: IconButton(
                           padding: const EdgeInsets.all(2),
                           color: const Color.fromARGB(255, 244, 246, 249),
-                          icon: const Icon(Icons.light_mode_rounded,
-                              color: Color.fromARGB(255, 224, 44, 32)),
-                          onPressed: () {},
+                          icon: Icon(
+                            Icons.light_mode_rounded,
+                            color: bike != null
+                                ? !bike!.LightState
+                                    ? Color.fromARGB(255, 81, 66, 64)
+                                    : Color.fromARGB(255, 224, 44, 32)
+                                : Color.fromARGB(255, 224, 44, 32),
+                          ),
+                          onPressed: () {
+                            bikeService.ChangeLightState(true).then((value) => setState(() {
+                                      bike = bikeService.bikeData!;
+                                    }));
+                          },
                         ),
                       ),
                       const Text(
@@ -146,13 +235,27 @@ class ScooterManagement extends StatelessWidget {
                           backgroundColor:
                               const Color.fromARGB(255, 254, 254, 254),
                         ),
-                        onPressed: () {},
+                        onPressed: () {
+                          bikeService.ChangeAlarmState(false).then((value) => setState(() {
+                                      bike = bikeService.bikeData!;
+                                    }));
+                        },
                         child: IconButton(
                           padding: const EdgeInsets.all(2),
                           color: const Color.fromARGB(255, 244, 246, 249),
-                          icon: const Icon(Icons.notifications_off,
-                              color: Color.fromARGB(255, 224, 44, 32)),
-                          onPressed: () {},
+                          icon: Icon(
+                            Icons.notifications_off,
+                            color: bike != null
+                                ? bike!.AlarmState
+                                    ? Color.fromARGB(255, 81, 66, 64)
+                                    : Color.fromARGB(255, 224, 44, 32)
+                                : Color.fromARGB(255, 224, 44, 32),
+                          ),
+                          onPressed: () {
+                            bikeService.ChangeAlarmState(false).then((value) => setState(() {
+                                      bike = bikeService.bikeData!;
+                                    }));
+                          },
                         ),
                       ),
                       const Text(
@@ -173,13 +276,27 @@ class ScooterManagement extends StatelessWidget {
                           backgroundColor:
                               const Color.fromARGB(255, 254, 254, 254),
                         ),
-                        onPressed: () {},
+                        onPressed: () {
+                          bikeService.ChangeAlarmState(true).then((value) => setState(() {
+                                      bike = bikeService.bikeData!;
+                                    }));
+                        },
                         child: IconButton(
                           padding: const EdgeInsets.all(2),
                           color: const Color.fromARGB(255, 244, 246, 249),
-                          icon: const Icon(Icons.notifications_on,
-                              color: Color.fromARGB(255, 224, 44, 32)),
-                          onPressed: () {},
+                          icon: Icon(
+                            Icons.notifications_on,
+                            color: bike != null
+                                ? !bike!.AlarmState
+                                    ? Color.fromARGB(255, 81, 66, 64)
+                                    : Color.fromARGB(255, 224, 44, 32)
+                                : Color.fromARGB(255, 224, 44, 32),
+                          ),
+                          onPressed: () {
+                            bikeService.ChangeAlarmState(true).then((value) => setState(() {
+                                      bike = bikeService.bikeData!;
+                                    }));
+                          },
                         ),
                       ),
                       const Text(
@@ -194,7 +311,7 @@ class ScooterManagement extends StatelessWidget {
             ],
           ),
         ],
-      ),
-    );
+      );
+    }
   }
 }
