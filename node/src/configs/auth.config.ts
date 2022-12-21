63ef48a319edfd1a13dac31090b93aa10bee5456
kamkoum-sabrine <kamkoumsabrine30@gmail.com>
2022-12-21 23:43:08 +0100

    [node] protect accept and refuse users methods with roles

--- node/src/configs/auth.config.ts
+++ node/src/configs/auth.config.ts
@@ -4,7 +4,7 @@ import User from "../models/user.model";
 
 
 export const fetchUser = async (req: Request, res: Response, next: NextFunction) => {
-	if (req.path.includes("role") || req.path.includes("register") || req.path.includes("accept") || req.path.includes("refuse")) {
+	if (req.path.includes("role") || req.path.includes("register")) {
 		next();
 	} else {
 		let token = req.header('AutorizationNode');
@@ -17,7 +17,6 @@ export const fetchUser = async (req: Request, res: Response, next: NextFunction)
 			for (let index = 0; index < req.body.user.roles.length; index++) {
 				const element = req.body.user.roles[index];
 				let fetchRole = await Role.find({ _id: element._id }, async (err: Error, roleUser: any) => {
-					console.log(element.status)
 
 					if (element.status == 1) {
 						if (roleUser[0].name == "Admin") {
