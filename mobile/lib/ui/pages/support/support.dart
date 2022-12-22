97440ab84530b40720b6ad469991f0847752a076
kamkoum-sabrine <kamkoumsabrine30@gmail.com>
2022-12-22 19:23:11 +0100

    [flutter] create form add claims

--- mobile/lib/ui/pages/support/support.dart
+++ mobile/lib/ui/pages/support/support.dart
@@ -13,17 +13,6 @@ class Support extends StatefulWidget {
 class _Support extends State<Support> {
   String password = "";
   var index = 0;
-  // List<Widget> pages = <Widget>[];
-  // @override
-  // void initState() {
-  //   super.initState();
-  //   pages = [
-  //     const Managment(),
-  //     const Localization(),
-  //     const Settings(),
-  //     const Notifications()
-  //   ];
-  // }
 
   _Support();
 
@@ -39,18 +28,78 @@ class _Support extends State<Support> {
             ),
             backgroundColor: const Color.fromARGB(255, 19, 27, 54),
           ),
-          body: Text("Hello world"),
-          // body: SingleChildScrollView(
-          //     child: Padding(
-          //   padding: const EdgeInsets.all(40),
-          //   child: FadeIndexedStack(
-          //     key: ValueKey(1),
-          //     index: index,
-          //     children: pages,
-          //   ),
-          // )),
+          body: SingleChildScrollView(
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                const Padding(
+                  padding: EdgeInsets.all(40),
+                  child: Text(
+                    'Send reclamation',
+                    style: TextStyle(
+                        fontSize: 24.0,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w500),
+                  ),
+                ),
+                const Padding(
+                    padding: EdgeInsets.all(20),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white,
+                        hintText: 'Subject',
+                      ),
+                    )),
+                const Padding(
+                    padding: EdgeInsets.all(20),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white,
+                        hintText: 'Title',
+                      ),
+                    )),
+                const Padding(
+                  padding: EdgeInsets.all(20),
+                  child: SizedBox(
+                    width: 400.0,
+                    height: 200.0,
+                    child: TextField(
+                      maxLines: 10,
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white,
+                        hintText: 'Type here...',
+                      ),
+                    ),
+                  ),
+                ),
+                ElevatedButton.icon(
+                  style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all<Color>(
+                          Color.fromARGB(224, 0, 119, 182)),
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18.0),
+                      ))),
+                  label: const Text(
+                    "Send",
+                    style: TextStyle(color: Color.fromARGB(255, 255, 253, 253)),
+                  ),
+                  icon: const Icon(Icons.add,
+                      color: Color.fromARGB(255, 255, 255, 255), size: 18),
+                  onPressed: () {},
+                ),
+              ],
+            ),
+          ),
           backgroundColor: const Color.fromARGB(255, 19, 27, 54),
           drawer: NavDrawerDemo(widget.user),
         )));
+  }
+
+  hello() {
+    print("object");
   }
 }
