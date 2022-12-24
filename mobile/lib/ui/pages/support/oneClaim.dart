855e011d3b3c778d8670bf2b03999c4295805882
kamkoum-sabrine <kamkoumsabrine30@gmail.com>
2022-12-24 20:11:15 +0100

    [flutter] create OneClaim widget

---
+++ mobile/lib/ui/pages/support/oneClaim.dart
@@ -0,0 +1,46 @@
+import 'package:mobile/imports.dart';
+import 'package:mobile/models/Claim.dart';
+
+class getOneClaim extends StatelessWidget {
+  late List<Claim> claims;
+  getOneClaim(this.claims);
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+        child: ListView.builder(
+      // scrollDirection: Axis.vertical,
+      shrinkWrap: true,
+      padding: const EdgeInsets.all(8),
+      itemCount: claims.length,
+      itemBuilder: (BuildContext context, int index) {
+        final claim = claims[index];
+        return Stack(children: [
+          Center(
+            child: Card(
+              color: Color.fromARGB(255, 29, 39, 70),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  ListTile(
+                    leading: Icon(
+                      Icons.circle,
+                      color: Colors.red,
+                      size: 50,
+                    ),
+                    title: Text(
+                      "${claim.title}",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    subtitle: Text('14-12-2022',
+                        style: TextStyle(color: Colors.white)),
+                  ),
+                ],
+              ),
+            ),
+          )
+        ]);
+      },
+      // separatorBuilder: (BuildContext context, int index) => const Divider(),
+    ));
+  }
+}
