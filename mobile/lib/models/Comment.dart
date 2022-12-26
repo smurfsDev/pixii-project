8121ff773c99165e700caa60095fc4bf6467c615
kamkoum-sabrine <kamkoumsabrine30@gmail.com>
2022-12-27 00:14:31 +0100

    [flutter] create Comment class

---
+++ mobile/lib/models/Comment.dart
@@ -0,0 +1,47 @@
+import 'dart:convert';
+import 'dart:ffi';
+import 'dart:html';
+
+import 'package:mobile/models/Status.dart';
+
+Comment claimFromJson(String str) => Comment.fromJson(json.decode(str));
+String commentToJson(Comment data) => json.encode(data.toJson());
+
+class Comment {
+  String? id;
+  String message;
+  String user;
+  String? claim;
+  String? created;
+
+  Comment(
+      {required this.id,
+      required this.message,
+      required this.user,
+      required this.claim,
+      required this.created});
+  factory Comment.fromJson(Map<String, dynamic> json) {
+    return Comment(
+      id: json['_id'] as String?,
+      message: json['message'] as String,
+      user: json['user'] as String,
+      claim: json['claim'] as String,
+      created: json['created'] as String?,
+    );
+  }
+
+  Map toJson() {
+    return {
+      'id': id,
+      'message': message,
+      'user': user,
+      'claim': claim,
+      'created': created,
+    };
+  }
+
+  @override
+  String toString() {
+    return 'Comment{id: $id, message: $message, user: $user, claim: $claim, created: $created}';
+  }
+}
