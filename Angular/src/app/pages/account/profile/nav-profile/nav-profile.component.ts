e7afde3f3e89cb3450fef055e894c4cf5eaff945
Mohamed Nour Bensalah <mednourbensalah@gmail.com>
2022-12-28 03:37:09 +0100

    [AG]Create profile nav

---
+++ Angular/src/app/pages/account/profile/nav-profile/nav-profile.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-profile',
+  templateUrl: './nav-profile.component.html',
+  styleUrls: ['./nav-profile.component.css']
+})
+export class NavProfileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
