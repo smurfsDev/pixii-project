a2e4c99bb7446e4c1e6c70c82ee4ab8f00418eed
Mohamed Nour Bensalah <mednourbensalah@gmail.com>
2022-12-28 02:47:37 +0100

    [AG] Add services of profile

--- Angular/src/app/service/account/profile.service.ts
+++ Angular/src/app/service/account/profile.service.ts
@@ -6,7 +6,33 @@ import { environment } from 'src/environments/environment';
   providedIn: 'root',
 })
 export class ProfileService {
-
   constructor(private http: HttpClient) {}
 
+  getProfile() {
+    return this.http.get(`${environment.apiUrl}/profile`);
+  }
+
+  getScooterName() {
+    return this.http.get(`${environment.apiUrl}/gscootername`);
+  }
+
+  updateProfile(data: any) {
+    return this.http.put(`${environment.apiUrl}/profilemodify`, data);
+  }
+
+  updatePassword(data: any) {
+    return this.http.put(`${environment.apiUrl}/changepassword`, data);
+  }
+
+  updateScooterName(data: any) {
+    return this.http.put(`${environment.apiUrl}/scootername`, data);
+  }
+
+  uploadProfilePicture(data: any) {
+    return this.http.post(`${environment.apiUrl}/upload`, data);
+  }
+
+  getProfilePicture() {
+    return this.http.get(`${environment.apiUrl}/getimage`);
+  }
 }
