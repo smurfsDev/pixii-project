a73d0a013fef3703312dead7e04a0c783a730c2b
Mohamed Nour Bensalah <mednourbensalah@gmail.com>
2022-12-28 13:56:46 +0100

    [SP]FIX Controller

--- microservice-application/spring-auth/src/main/java/com/javainuse/controller/UserProfileController.java
+++ microservice-application/spring-auth/src/main/java/com/javainuse/controller/UserProfileController.java
@@ -9,13 +9,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
-
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import com.javainuse.config.WebSecurityConfig;
 import com.javainuse.entities.User;
@@ -26,6 +26,8 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import net.bytebuddy.utility.RandomString;
 import net.minidev.json.JSONObject;
 
+@RestController
+@CrossOrigin
 public class UserProfileController {
     @Autowired
     private UserRepository userrepository;
