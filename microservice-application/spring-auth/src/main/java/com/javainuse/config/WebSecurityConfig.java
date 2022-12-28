ff460834a10096431d482ce1d83e35703b186afe
Mohamed Nour Bensalah <mednourbensalah@gmail.com>
2022-12-28 22:37:47 +0100

    [SP]permission

--- microservice-application/spring-auth/src/main/java/com/javainuse/config/WebSecurityConfig.java
+++ microservice-application/spring-auth/src/main/java/com/javainuse/config/WebSecurityConfig.java
@@ -60,10 +60,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				.antMatchers("/forgot_password").permitAll()
 				.antMatchers("/reset_password").permitAll()
 				.antMatchers("/roles").permitAll().antMatchers("/checkEmail/{email}").permitAll()
-        .antMatchers("/accept/{idUser}/{idRole}").hasAnyAuthority("Admin", "Super Admin")
+				.antMatchers("/accept/{idUser}/{idRole}").hasAnyAuthority("Admin", "Super Admin")
 				.antMatchers("/reject/{idUser}/{idRole}").hasAnyAuthority("Admin", "Super Admin")
 				.antMatchers("/users").hasAnyAuthority("Admin", "Super Admin")
 				.antMatchers("/checkUsername/{username}").permitAll().antMatchers("/resend").permitAll()
+				.antMatchers("/profilemodify").permitAll()
 				// all other requests need to be authenticated
 				.anyRequest().authenticated().and().
 				// make sure we use stateless session; session won't be used to
