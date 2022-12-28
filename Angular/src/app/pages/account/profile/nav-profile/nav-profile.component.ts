c95bfcbcff4be3e6bd83a3a4449fea3b232e3f34
Mohamed Nour Bensalah <mednourbensalah@gmail.com>
2022-12-28 03:50:30 +0100

    [AG] Add profile nav com

--- Angular/src/app/pages/account/profile/nav-profile/nav-profile.component.ts
+++ Angular/src/app/pages/account/profile/nav-profile/nav-profile.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { ProfileService } from '../../../../service/account/profile.service';
 
 @Component({
   selector: 'app-nav-profile',
   templateUrl: './nav-profile.component.html',
-  styleUrls: ['./nav-profile.component.css']
+  styleUrls: ['./nav-profile.component.scss'],
 })
 export class NavProfileComponent implements OnInit {
+  profile: any;
+  name: any;
 
-  constructor() { }
+  constructor(private ProfileService: ProfileService) {}
 
   ngOnInit(): void {
+    this.getProfilePicture();
+    this.getProfile();
   }
 
+  getProfilePicture() {
+    this.ProfileService.getProfilePicture().subscribe((data: any) => {
+      this.profile = data;
+    });
+  }
+  getProfile() {
+    this.ProfileService.getProfile().subscribe((data: any) => {
+      this.name = data;
+    });
+  }
 }
