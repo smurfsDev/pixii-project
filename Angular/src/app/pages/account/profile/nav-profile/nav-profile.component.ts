0484ded6dbc3d3c0f8022d25c990f863464c7733
Mohamed Nour Bensalah <mednourbensalah@gmail.com>
2022-12-28 04:02:29 +0100

    [Ag]Modifie nav-app TS

--- Angular/src/app/pages/account/profile/nav-profile/nav-profile.component.ts
+++ Angular/src/app/pages/account/profile/nav-profile/nav-profile.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ProfileService } from '../../../../service/account/profile.service';
+import { Router } from '@angular/router';
+import { User } from '../../../../models/user';
+import { LogoutAction } from 'src/app/store/auth/actions';
+import { Store } from '@ngxs/store';
 
 @Component({
   selector: 'app-nav-profile',
@@ -10,13 +14,20 @@ export class NavProfileComponent implements OnInit {
   profile: any;
   name: any;
 
-  constructor(private ProfileService: ProfileService) {}
+  constructor(
+    private ProfileService: ProfileService,
+    private Store: Store,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.getProfilePicture();
     this.getProfile();
   }
-
+  logout() {
+    this.Store.dispatch(new LogoutAction());
+    this.router.navigate(['/auth']);
+  }
   getProfilePicture() {
     this.ProfileService.getProfilePicture().subscribe((data: any) => {
       this.profile = data;
