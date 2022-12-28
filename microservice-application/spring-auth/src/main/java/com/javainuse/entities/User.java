8f8e4a044ac0433365221fa2f66ec48fe021c3a8
Mohamed Nour Bensalah <mednourbensalah@gmail.com>
2022-12-28 01:51:19 +0100

    [SP] UPdate user

--- microservice-application/spring-auth/src/main/java/com/javainuse/entities/User.java
+++ microservice-application/spring-auth/src/main/java/com/javainuse/entities/User.java
@@ -19,7 +19,6 @@ import javax.persistence.JoinTable;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToMany;
 import javax.persistence.PrePersist;
-import javax.persistence.UniqueConstraint;
 
 @Entity
 public class User {
