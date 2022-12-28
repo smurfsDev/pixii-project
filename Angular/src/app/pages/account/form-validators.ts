8c3cc6056fcc85627a9105ce4f81abc10fef33be
Mohamed Nour Bensalah <mednourbensalah@gmail.com>
2022-12-28 03:13:48 +0100

    [AG] Create validators

--- Angular/src/app/pages/account/form-validators.ts
+++ Angular/src/app/pages/account/form-validators.ts
@@ -1,11 +1,7 @@
-import { AbstractControl, ValidationErrors, ValidatorFn } from "@angular/forms";
+import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
 
-export function checkPassword(
-  matchTo: string,
-  reverse?: boolean
-): ValidatorFn {
-  return (control: AbstractControl):
-  ValidationErrors | null => {
+export function checkPassword(matchTo: string, reverse?: boolean): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
     if (control.parent && reverse) {
       const c = (control.parent?.controls as any)[matchTo] as AbstractControl;
       if (c) {
@@ -15,8 +11,66 @@ export function checkPassword(
     }
     return !!control.parent &&
       !!control.parent.value &&
-      control.value ===
-      (control.parent?.controls as any)[matchTo].value
+      control.value === (control.parent?.controls as any)[matchTo].value
+      ? null
+      : { matching: true };
+  };
+}
+export function checkOldPassword(
+  matchTo: string,
+  reverse?: boolean
+): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    if (control.parent && reverse) {
+      const c = (control.parent?.controls as any)[matchTo] as AbstractControl;
+      if (c) {
+        c.updateValueAndValidity();
+      }
+      return null;
+    }
+    return !!control.parent &&
+      !!control.parent.value &&
+      control.value !== (control.parent?.controls as any)[matchTo].value
+      ? null
+      : { matching: true };
+  };
+}
+
+export function checkPasswordPattern(
+  matchTo: string,
+  reverse?: boolean
+): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    if (control.parent && reverse) {
+      const c = (control.parent?.controls as any)[matchTo] as AbstractControl;
+      if (c) {
+        c.updateValueAndValidity();
+      }
+      return null;
+    }
+    return !!control.parent &&
+      !!control.parent.value &&
+      control.value.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]+$/)
+      ? null
+      : { matching: true };
+  };
+}
+
+export function checkOldPasswordPattern(
+  matchTo: string,
+  reverse?: boolean
+): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    if (control.parent && reverse) {
+      const c = (control.parent?.controls as any)[matchTo] as AbstractControl;
+      if (c) {
+        c.updateValueAndValidity();
+      }
+      return null;
+    }
+    return !!control.parent &&
+      !!control.parent.value &&
+      control.value !== (control.parent?.controls as any)[matchTo].value
       ? null
       : { matching: true };
   };
