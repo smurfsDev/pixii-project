20feace63a551c1492fd14a07d0746a76eeaefdd
Mohamed Nour Bensalah <mednourbensalah@gmail.com>
2022-12-28 02:46:48 +0100

    [AG] create profile service

---
+++ Angular/src/app/service/account/profile.service.ts
@@ -0,0 +1,12 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProfileService {
+
+  constructor(private http: HttpClient) {}
+
+}
