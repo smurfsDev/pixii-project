f0a8907d6f320b8748929a9534442ef33c5094d1
youssefchlendi <youssef.chlendi@gmail.com>
2023-01-06 19:31:29 +0100

    add no claims message

--- mobile/lib/ui/pages/support/myClaims.dart
+++ mobile/lib/ui/pages/support/myClaims.dart
@@ -2,6 +2,7 @@ import 'package:mobile/imports.dart';
 import 'package:mobile/models/Claim.dart';
 import 'package:mobile/service/claims.dart';
 import 'package:mobile/ui/pages/support/oneClaim.dart';
+import 'package:mobile/ui/pages/support/support.dart';
 
 // ignore: must_be_immutable
 class MyClaims extends StatefulWidget {
@@ -38,19 +39,86 @@ class _MyClaims extends State<MyClaims> {
             backgroundColor: const Color.fromARGB(255, 19, 27, 54),
           ),
           body: Padding(
-            padding: EdgeInsets.only(top: 10),
+            padding: const EdgeInsets.only(top: 10),
             child: FutureBuilder(
               future: ClaimsService().getMyClaims(),
               builder: ((context, snapshot) {
-                print(snapshot.data);
-                if (snapshot.hasData) {
+                if (snapshot.data?.length == 0) {
+                  return
+                      Container(
+                    padding: const EdgeInsets.only(top: 20),
+                    child: Center(
+                      child: Column(
+                        children: [
+                          const Icon(
+                            Icons.error,
+                            color: Colors.red,
+                            size: 50,
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          const Text(
+                            "No claims found",
+                            style: TextStyle(color: Colors.white, fontSize: 20),
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          const Padding(
+                            padding: EdgeInsets.only(left: 20, right: 20),
+                            child: Text(
+                              "You can create a new claim by clicking on the button below",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.w200,),
+                                  textAlign: TextAlign.center,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          ElevatedButton.icon(
+                            style: ButtonStyle(
+                              padding: MaterialStateProperty.all<EdgeInsets>(
+                                  const EdgeInsets.only(
+                                      left: 20, right: 20, top: 10, bottom: 10)),
+                                backgroundColor:
+                                    MaterialStateProperty.all<Color>(
+                                        const Color.fromARGB(223, 7, 133, 200)),
+                                shape: MaterialStateProperty.all<
+                                        RoundedRectangleBorder>(
+                                    RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(18.0),
+                                ))),
+                            label: const Text(
+                              "Create new claim",
+                              style: TextStyle(
+                                  color: Color.fromARGB(255, 255, 253, 253)),
+                            ),
+                            icon: const Icon(Icons.add,
+                                color: Color.fromARGB(255, 255, 255, 255),
+                                size: 18),
+                            onPressed: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => Support(user)));
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                } else if (snapshot.hasData) {
                   return getOneClaim(snapshot.data!, user);
                 } else if (snapshot.hasError) {
                   return Text('${snapshot.error}');
                 }
 
-                return Center(
-                  child: const CircularProgressIndicator(),
+                return const Center(
+                  child: CircularProgressIndicator(),
                 );
               }),
             ),
