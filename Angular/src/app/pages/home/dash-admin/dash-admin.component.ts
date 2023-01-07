076f00289558dce88d7188af23fe6ce2305c4fdc
youssefchlendi <youssef.chlendi@gmail.com>
2023-01-07 17:55:56 +0100

    get callbacks and manage them

--- Angular/src/app/pages/home/dash-admin/dash-admin.component.ts
+++ Angular/src/app/pages/home/dash-admin/dash-admin.component.ts
@@ -1,22 +1,74 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Callback } from 'src/app/models/callback.model';
+import { CallbackService } from 'src/app/service/callback/callback.service';
 import { DashboardService } from 'src/app/service/dashboard/dashboard.service';
 
 @Component({
-  selector: 'app-dash-admin',
-  templateUrl: './dash-admin.component.html',
-  styleUrls: ['./dash-admin.component.scss']
+	selector: 'app-dash-admin',
+	templateUrl: './dash-admin.component.html',
+	styleUrls: ['./dash-admin.component.scss']
 })
 export class DashAdminComponent implements OnInit {
 
-    @Input() options: any;
-    @Input() claimsByStatus: any;
-    @Input() percentage = 0;
-    @Input() bikes= 0;
-    @Input() savManagers = 0;
-    @Input() savTechnicians = 0;
-    @Input() scooterOwners = 0;
-    ngOnInit(): void {
-    }
+	@Input() options: any;
+	@Input() claimsByStatus: any;
+	@Input() percentage = 0;
+	@Input() bikes = 0;
+	@Input() savManagers = 0;
+	@Input() savTechnicians = 0;
+	@Input() scooterOwners = 0;
+
+	callbacks: Callback[] = [];
+
+	constructor(private callback: CallbackService, private snackbar: MatSnackBar, public dialog: MatDialog) { }
+
+	ngOnInit(): void {
+		this.fetchCallbacks();
+	}
+
+	fetchCallbacks() {
+		this.callback.getCallbacks().subscribe((data: any) => {
+			this.callbacks = data;
+		});
+	}
+
+
+	changeCallbackCalledStatus(id: string, status: boolean) {
+		this.dialog.open(CalledDialogComponent,
+		).afterClosed().subscribe((choice: string) => {
+			if (choice == 'yes') {
+				this.callback.changeCallbackCalledStatus(id, status).subscribe((data: any) => {
+					this.fetchCallbacks();
+					this.snackbar.open('Callback status changed', 'Close', {
+						duration: 3000,
+						verticalPosition: 'bottom',
+						horizontalPosition: 'right'
+					});
+				});
+			}else{
+				this.snackbar.open('Callback status not changed', 'Close', {
+					duration: 3000,
+					verticalPosition: 'bottom',
+					horizontalPosition: 'right'
+				});
+			}
+		});
+	}
+
 
 
 }
+
+@Component({
+	selector: 'called-dialog',
+	templateUrl: './called-dialog.component.html',
+})
+export class CalledDialogComponent {
+	constructor(public dialogRef: MatDialogRef<CalledDialogComponent>) { }
+	onClick(choice: string): void {
+		this.dialogRef.close(choice);
+	}
+}
+
--- Angular/src/app/service/callback/callback.service.ts
+++ Angular/src/app/service/callback/callback.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Callback } from 'src/app/models/callback.model';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
@@ -15,6 +17,10 @@ export class CallbackService {
 
   getCallbacks(){
 	return this._http.get(`${environment.apiUrl}/node/callback`);
+  }
+
+  changeCallbackCalledStatus(id:string, status:boolean){
+	return this._http.put(`${environment.apiUrl}/node/callback/${id}`, {called:status});
   }
   
 
