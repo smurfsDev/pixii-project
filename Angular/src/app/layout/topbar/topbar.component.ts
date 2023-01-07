36619f9df00c6ca23774c591d4406b41a5a13a1f
Mohamed Nour Bensalah <mednourbensalah@gmail.com>
2023-01-07 16:18:54 +0100

    [AG] Dynamic pp and user name

--- Angular/src/app/layout/topbar/topbar.component.ts
+++ Angular/src/app/layout/topbar/topbar.component.ts
@@ -3,46 +3,70 @@ import { Store } from '@ngxs/store';
 import { User } from 'src/app/models/user';
 import { LogoutAction } from 'src/app/store/auth/actions';
 import { Router } from '@angular/router';
+import { ProfileService } from '../../service/account/profile.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-topbar',
   templateUrl: './topbar.component.html',
-  styleUrls: ['./topbar.component.scss']
+  styleUrls: ['./topbar.component.scss'],
 })
 export class TopbarComponent implements OnInit {
   vehicle = false;
   location = false;
   controlPanel = false;
   isScooterOwner = false;
+  profile: any;
+  name: any;
+  env = environment.apiUrl;
+
   @Output() value = new EventEmitter<any>();
   BtnVehicule = {
     color: '#ffffffa4',
     fontSize: '18px',
-  }
+  };
   BtnLocation = {
     color: '#ffffffa4',
     fontSize: '18px',
-  }
+  };
   BtnControlPanel = {
     color: '#ffffffa4',
     fontSize: '18px',
-  }
+  };
 
-  constructor(private store: Store,private router:Router) { }
+  constructor(
+    private ProfileService: ProfileService,
+    private store: Store,
+    private router: Router
+  ) {}
 
   authUser: User | undefined;
   ngOnInit(): void {
-    this.store.select(state => state.AuthState).subscribe(user => {
-      if (user) {
-        this.authUser = user.user;
-        this.isScooterOwner = user['isScooterOwner'].isScooterOwner;
-      }
+    this.store
+      .select((state) => state.AuthState)
+      .subscribe((user) => {
+        if (user) {
+          this.authUser = user.user;
+          this.isScooterOwner = user['isScooterOwner'].isScooterOwner;
+        }
+      });
+    this.getProfilePicture();
+    this.getProfile();
+  }
+  getProfilePicture() {
+    this.ProfileService.getProfilePicture().subscribe((data: any) => {
+      this.profile = data;
+    });
+  }
+  getProfile() {
+    this.ProfileService.getProfile().subscribe((data: any) => {
+      this.name = data;
     });
   }
 
   logout() {
-	this.store.dispatch(new LogoutAction());
-	this.router.navigate(['/auth']);
+    this.store.dispatch(new LogoutAction());
+    this.router.navigate(['/auth']);
   }
   vehicule() {
     this.initBtn();
@@ -52,11 +76,11 @@ export class TopbarComponent implements OnInit {
     this.BtnVehicule = {
       color: '#fff',
       fontSize: '21px',
-    }
+    };
     this.values();
   }
-  Location(){
-        this.initBtn();
+  Location() {
+    this.initBtn();
 
     this.vehicle = false;
     this.location = true;
@@ -64,11 +88,11 @@ export class TopbarComponent implements OnInit {
     this.BtnLocation = {
       color: '#fff',
       fontSize: '21px',
-    }
+    };
     this.values();
   }
-  ControlPanel(){
-        this.initBtn();
+  ControlPanel() {
+    this.initBtn();
 
     this.vehicle = false;
     this.location = false;
@@ -76,30 +100,29 @@ export class TopbarComponent implements OnInit {
     this.BtnControlPanel = {
       color: '#fff',
       fontSize: '21px',
-    }
+    };
     this.values();
     // console.log(this.vehicle,this.location,this.controlPanel);
   }
-  initBtn(){
+  initBtn() {
     this.BtnVehicule = {
       color: '#ffffffa4',
       fontSize: '18px',
-    }
+    };
     this.BtnLocation = {
       color: '#ffffffa4',
       fontSize: '18px',
-    }
+    };
     this.BtnControlPanel = {
       color: '#ffffffa4',
       fontSize: '18px',
-    }
+    };
   }
-  values(){
+  values() {
     this.value.emit({
-      vehicle:this.vehicle,
-      location:this.location,
-      controlPanel:this.controlPanel
-    })
+      vehicle: this.vehicle,
+      location: this.location,
+      controlPanel: this.controlPanel,
+    });
   }
-
 }
