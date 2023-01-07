9d5e709b448b122924d8dd4a064c057b8019a422
youssefchlendi <youssef.chlendi@gmail.com>
2023-01-07 11:58:48 +0100

    add change lock/light/alarm state to bikeService

--- Angular/src/app/service/bike/bike-data.service.ts
+++ Angular/src/app/service/bike/bike-data.service.ts
@@ -14,4 +14,17 @@ export class BikeDataService {
   getMyBikeData(id:string):Observable<Bike> {
 	return this._http.get(`${environment.apiUrl}/node/bike/${id}`) as Observable<Bike>;
   }
+
+  changeLockState(id:string, state:boolean):Observable<Bike> {
+	return this._http.put(`${environment.apiUrl}/node/bike/lock/${id}`, {TheftState:state}) as Observable<Bike>;
+  }
+
+  changeLightState(id:string, state:boolean):Observable<Bike> {
+	return this._http.put(`${environment.apiUrl}/node/bike/light/${id}`, {LightState:state}) as Observable<Bike>;
+  }
+
+  changeAlarmState(id:string, state:boolean):Observable<Bike> {
+	return this._http.put(`${environment.apiUrl}/node/bike/alarm/${id}`, {AlarmState:state}) as Observable<Bike>;
+  }
+
 }
