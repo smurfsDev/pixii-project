482c599e6d745be25aa1342f9676a3a65b29bbff
Mohamed Nour Bensalah <mednourbensalah@gmail.com>
2023-01-08 02:46:40 +0100

    [ft]

--- mobile/lib/ui/pages/user/update_profile.dart
+++ mobile/lib/ui/pages/user/update_profile.dart
@@ -1,30 +1,62 @@
 import 'dart:io';
-
+import 'package:http/http.dart' as http;
 import 'package:get/get.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:mobile/imports.dart';
+import 'package:mobile/models/profile.dart';
 import 'package:mobile/service/user_profile.dart';
 import 'package:mobile/ui/pages/user/widget/appbar_widget.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart';
+import 'package:async/async.dart';
+import 'dart:convert';
 
 class EditProfile extends StatefulWidget {
-  User user;
-  EditProfile(this.user, {super.key});
+  String email;
+  String name;
+  String username;
+  EditProfile(this.name, this.email, this.username, {super.key});
 
   @override
-  State<EditProfile> createState() => _EditProfile(this.user);
+  State<EditProfile> createState() => _EditProfile();
 }
 
 class _EditProfile extends State<EditProfile> {
-  User user;
   final _formKey = GlobalKey<FormState>();
   String email = "";
   String name = "";
-  // String confirmemail = "";
+  String username = "";
+  String oldName = "";
+  String oldEmail = "";
   bool loading = false;
-  _EditProfile(this.user);
   File? pickedImage;
+  String? imagePath;
+  late AuthService userService;
+  @override
+  void initState() {
+    super.initState();
+    setState(() {
+      email = widget.email;
+      name = widget.name;
+      oldName = widget.name;
+    });
+    userService = AuthService();
+    userService.loadSettings();
+    User? user = userService.user;
+    var storage = const FlutterSecureStorage();
+    print(user?.image);
+
+    fetchProfile();
+
+    WidgetsBinding.instance.addPostFrameCallback((_) async {
+      var usr = await storage.read(key: "user");
+      print(usr);
+      var user = User.fromJson(jsonDecode(usr!));
+      setState(() {
+        imagePath = user.image;
+      });
+    });
+  }
 
   Future<File> saveImagePermantly(String path) async {
     final directory = await getApplicationDocumentsDirectory();
@@ -33,16 +65,41 @@ class _EditProfile extends State<EditProfile> {
     return File(path).copy(permImage.path);
   }
 
-  pickImage(ImageSource imageType) async {
+  pickImage(ImageSource imageType, BuildContext context) async {
     try {
       final photo = await ImagePicker().pickImage(source: imageType);
       if (photo == null) return;
 
       final permImage = await saveImagePermantly(photo.path);
       final tempImage = File(photo.path);
+
       setState(() {
         pickedImage = permImage;
+        fetchProfile();
       });
+
+      var userProfilService = UserPorfileService();
+      try {
+        await userProfilService.updateProfileImage(permImage);
+        final profile = await userProfilService.getProfile();
+        var storage = const FlutterSecureStorage();
+        var usr = await storage.read(key: "userprofile");
+        var user = ProfileModel.fromJson(jsonDecode(usr!));
+        setState(() {
+          imagePath = user.image;
+        });
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text("Profile image updated"),
+          ),
+        );
+      } catch (e) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text("Error updating profile image"),
+          ),
+        );
+      }
 
       Get.back();
     } catch (error) {
@@ -56,9 +113,12 @@ class _EditProfile extends State<EditProfile> {
     return Scaffold(
       appBar: buildAppBar(context),
       body: ListView(
-        padding: EdgeInsets.symmetric(horizontal: 32),
-        physics: BouncingScrollPhysics(),
+        padding: const EdgeInsets.symmetric(horizontal: 32),
+        physics: const BouncingScrollPhysics(),
         children: [
+          const SizedBox(
+            height: 20,
+          ),
           Align(
             alignment: Alignment.center,
             child: Stack(
@@ -71,19 +131,26 @@ class _EditProfile extends State<EditProfile> {
                     ),
                   ),
                   child: ClipOval(
-                    child: pickedImage != null
-                        ? Image.file(
-                            pickedImage!,
+                    child: imagePath != null
+                        ? Image.network(
+                            Environment.apiUrl + "/user-photos/" + imagePath!,
                             width: 128,
                             height: 128,
                             fit: BoxFit.cover,
                           )
-                        : Image.network(
-                            'https://upload.wikimedia.org/wikipedia/commons/5/5f/Alberto_conversi_profile_pic.jpg',
-                            width: 128,
-                            height: 128,
-                            fit: BoxFit.cover,
-                          ),
+                        : pickedImage != null
+                            ? Image.file(
+                                pickedImage!,
+                                width: 128,
+                                height: 128,
+                                fit: BoxFit.cover,
+                              )
+                            : Image.network(
+                                'https://upload.wikimedia.org/wikipedia/commons/5/5f/Alberto_conversi_profile_pic.jpg',
+                                width: 128,
+                                height: 128,
+                                fit: BoxFit.cover,
+                              ),
                   ),
                 ),
                 Positioned(
@@ -114,14 +181,16 @@ class _EditProfile extends State<EditProfile> {
                                         ),
                                         ElevatedButton.icon(
                                           onPressed: () {
-                                            pickImage(ImageSource.camera);
+                                            pickImage(
+                                                ImageSource.camera, context);
                                           },
                                           icon: const Icon(Icons.camera),
                                           label: const Text("CAMERA"),
                                         ),
                                         ElevatedButton.icon(
                                           onPressed: () {
-                                            pickImage(ImageSource.gallery);
+                                            pickImage(
+                                                ImageSource.gallery, context);
                                           },
                                           icon: const Icon(Icons.image),
                                           label: const Text("GALLERY"),
@@ -149,7 +218,7 @@ class _EditProfile extends State<EditProfile> {
           const SizedBox(
             height: 8,
           ),
-          Text("@ ${user.username}",
+          Text("@ ${username}",
               style: const TextStyle(
                 fontSize: 17,
                 fontWeight: FontWeight.w600,
@@ -159,7 +228,7 @@ class _EditProfile extends State<EditProfile> {
           const SizedBox(
             height: 2,
           ),
-          Text(user.name,
+          Text(oldName,
               style: const TextStyle(
                 fontSize: 13,
                 fontWeight: FontWeight.w600,
@@ -174,22 +243,22 @@ class _EditProfile extends State<EditProfile> {
               child: Column(
                 children: [
                   TextFormField(
-                    initialValue: user.name,
+                    initialValue: name,
                     onChanged: (value) => {
                       setState(
                         () {
-                          user.name = value;
+                          name = value;
                         },
                       )
                     },
-                    style: TextStyle(color: Colors.white),
+                    style: const TextStyle(color: Colors.white),
                     decoration: InputDecoration(
-                      labelStyle: TextStyle(color: Colors.white),
-                        prefixIcon: const Icon(
+                      labelStyle: const TextStyle(color: Colors.white),
+                      prefixIcon: const Icon(
                         Icons.person,
                         color: Colors.white,
                       ),
-                      hintStyle: TextStyle(color: Colors.white),
+                      hintStyle: const TextStyle(color: Colors.white),
                       fillColor: Colors.white,
                       labelText: 'Full Name',
                       border: OutlineInputBorder(
@@ -203,24 +272,24 @@ class _EditProfile extends State<EditProfile> {
                       return null;
                     },
                   ),
-                  SizedBox(height: 20.0),
+                  const SizedBox(height: 20.0),
                   TextFormField(
-                    initialValue: user.email,
+                    initialValue: email,
                     onChanged: (value) => {
                       setState(
                         () {
-                          user.email = value;
+                          email = value;
                         },
                       )
                     },
-                    style: TextStyle(color: Colors.white),
+                    style: const TextStyle(color: Colors.white),
                     decoration: InputDecoration(
-                      labelStyle: TextStyle(color: Colors.white),
-                        prefixIcon: const Icon(
+                      labelStyle: const TextStyle(color: Colors.white),
+                      prefixIcon: const Icon(
                         Icons.email,
                         color: Colors.white,
                       ),
-                      hintStyle: TextStyle(color: Colors.white),
+                      hintStyle: const TextStyle(color: Colors.white),
                       fillColor: Colors.white,
                       labelText: 'Email',
                       border: OutlineInputBorder(
@@ -234,34 +303,7 @@ class _EditProfile extends State<EditProfile> {
                       return null;
                     },
                   ),
-                  SizedBox(height: 20.0),
-                  // TextFormField(
-                  //   initialValue: user.email,
-                  //   onChanged: (value) => {
-                  //     setState(
-                  //       () {
-                  //         confirmemail = value;
-                  //       },
-                  //     )
-                  //   },
-                  //   style: TextStyle(color: Colors.white),
-                  //   decoration: InputDecoration(
-                  //     labelStyle: TextStyle(color: Colors.white),
-                  //     hintStyle: TextStyle(color: Colors.white),
-                  //     fillColor: Colors.white,
-                  //     labelText: 'Confirm Email',
-                  //     border: OutlineInputBorder(
-                  //       borderRadius: BorderRadius.circular(10.0),
-                  //     ),
-                  //   ),
-                  //   validator: (value) {
-                  //     if (value == null || value.isEmpty) {
-                  //       return 'Please re-enter email';
-                  //     }
-                  //     return null;
-                  //   },
-                  // ),
-                  SizedBox(height: 20.0),
+                  const SizedBox(height: 20.0),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     crossAxisAlignment: CrossAxisAlignment.center,
@@ -269,11 +311,12 @@ class _EditProfile extends State<EditProfile> {
                     children: [
                       Expanded(
                         child: Container(
-                          margin: EdgeInsets.symmetric(horizontal: 70.0),
+                          margin: const EdgeInsets.symmetric(horizontal: 70.0),
                           child: ElevatedButton(
                             style: ElevatedButton.styleFrom(
                               backgroundColor: Colors.blue,
-                              padding: EdgeInsets.symmetric(vertical: 15.0),
+                              padding:
+                                  const EdgeInsets.symmetric(vertical: 15.0),
                               shape: RoundedRectangleBorder(
                                 borderRadius: BorderRadius.circular(18.0),
                               ),
@@ -282,10 +325,10 @@ class _EditProfile extends State<EditProfile> {
                               sendProfile(context, profileService);
                             },
                             child: loading
-                                ? CircularProgressIndicator(
+                                ? const CircularProgressIndicator(
                                     color: Colors.white,
                                   )
-                                : Text(
+                                : const Text(
                                     'update',
                                     style: TextStyle(
                                         fontSize: 14, color: Colors.white),
@@ -304,40 +347,45 @@ class _EditProfile extends State<EditProfile> {
     );
   }
 
-  void sendProfile(BuildContext context, UserPorfileService porfile) async {
-    final profileupdated = await porfile.updateProfile(user.name, user.email);
-    // setState(() {
-    //   loading = false;
-    // });
-    //  if (profileupdated) {
-    //   ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-    //   content: Text("Profile updated"),
-    //     backgroundColor: Color.fromARGB(255, 177, 177, 177),
-    // ));
-    // } else {
-    //   ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-    //     content: Text("Profile not updated"),
-    //     backgroundColor: Color.fromARGB(255, 177, 177, 177),
-    //   ));
-    // }
+  void fetchProfile() async {
+    var porfile = UserPorfileService();
+    final profile = await porfile.getProfile();
+    setState(() {
+      name = profile['name'];
+      email = profile['email'];
+      oldName = profile['name'];
+      username = profile['username'];
+      imagePath = profile['image'] ?? null;
+    });
   }
-  // print(user.name);
-  // print(user.email);
-  // print(confirmemail);
-  // if (user.email) {
-  // final profileupdated = await porfile.updateProfile(name, usemail);
 
-  // } else {
-  // ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-  //   content: Text("Email must match"),
-  //   backgroundColor: Colors.red,
-  // ));
-  // }
-  // if (_formKey.currentState!.validate()) {
-  //   setState(() {
-  //     loading = true;
-  //   });
-  // }
+  void sendProfile(BuildContext context, UserPorfileService porfile) async {
+    try {
+      final profileupdated = await porfile.updateProfile(name, email);
+      if (profileupdated == true) {
+        setState(() {
+          oldName = name;
+        });
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text(
+              'Profile Updated',
+              style: TextStyle(color: Colors.green),
+            ),
+          ),
+        );
+      }
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text(
+            'Error Updating Profile',
+            style: TextStyle(color: Colors.red),
+          ),
+        ),
+      );
+    }
+  }
 }
 
 class image_widget extends StatelessWidget {
--- mobile/lib/ui/pages/user/widget/appbar_widget.dart
+++ mobile/lib/ui/pages/user/widget/appbar_widget.dart
@@ -1,12 +1,23 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:mobile/imports.dart';
+import 'package:mobile/ui/pages/user/profile.dart';
 
 AppBar buildAppBar(BuildContext context) {
   // final icon = CupertinoIcons.moon_stars;
 
   return AppBar(
-    leading: BackButton(),
+    leading: BackButton(
+      onPressed: () {
+        Navigator.popUntil(context, (route) => route.isFirst);
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => ProfilePage(user: null),
+          ),
+        );
+            },
+    ),
     iconTheme: IconThemeData(color: Color.fromARGB(255, 255, 255, 255)),
     backgroundColor: Color.fromARGB(0, 255, 255, 255),
     elevation: 0,
